223cf9a9e8df293bae173e35b697406df26d285e
parviziAH <parviziAH@example.com>
2025-09-21 15:20:14 +0200

    add fede-in / out still adjusting navbar

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,9 +5,10 @@ const HeroSection: React.FC = () => {
   return (
     <section className="hero-section">
       <div className="hero-content">
-        <p className="hero-text">
+        <p className="hero-text-main">
           With love and gratitude, we invite you to share in the joy of our wedding day.
-          <br /><br />
+        </p>
+        <p className="hero-text-details">
           Friday, July 3rd, 2026<br />
           Podere Montale, Tuscany
         </p>
