4e665ed57ca3530feba8e13f8fa69de6fc12fe7e
parviziAH <parviziAH@example.com>
2025-09-21 09:45:23 +0200

    checkpoint: almost inserted initial transition

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import HeroSection from './components/HeroSection'
 import LinksSection from './components/LinksSection'
 import RegistrySection from './components/RegistrySection'
 import Footer from './components/Footer'
+import ScrollEffect from './components/ScrollEffect'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
-      <Navigation />
-      <main className="main">
-        <HeroSection />
-        <Header />
-        <LinksSection />
-        <RegistrySection />
-      </main>
-      <Footer />
+      <ScrollEffect>
+        <Navigation />
+        <main className="main">
+          <HeroSection />
+          <Header />
+          <LinksSection />
+          <RegistrySection />
+        </main>
+        <Footer />
+      </ScrollEffect>
     </div>
   )
 }
